bc71c6bb7551b63106c5b4a089a6a645e801ed08
LevSib2005 <kengymsen@gmail.com>
2025-11-18 11:43:42 +0700

    добавили shared-libs и sprints-service

--- backend/shared-libs/Backend.Shared/DTOs/SprintDto.cs
+++ backend/shared-libs/Backend.Shared/DTOs/SprintDto.cs
@@ -1,6 +1,11 @@
-﻿namespace DefaultNamespace;
+﻿namespace Backend.Shared.DTOs;
 
 public class SprintDto
 {
-    
+    public long Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Goal { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long ProjectId { get; set; }
 }
--- backend/shared-libs/Backend.Shared/Requests/CreateSprintRequest.cs
+++ backend/shared-libs/Backend.Shared/Requests/CreateSprintRequest.cs
@@ -1,6 +1,10 @@
-﻿namespace DefaultNamespace;
+﻿namespace Backend.Shared.Requests;
 
 public class CreateSprintRequest
 {
-    
+    public string Name { get; set; } = string.Empty;
+    public string? Goal { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public long ProjectId { get; set; }
 }
