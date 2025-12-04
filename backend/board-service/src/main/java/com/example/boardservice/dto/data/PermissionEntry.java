6f850c595d62d1f19982c7bb5bf76ee27bed3232
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-04 18:13:57 +0700

    boardservice: create dto for create/update role

--- backend/board-service/src/main/java/com/example/boardservice/dto/data/PermissionEntry.java
+++ backend/board-service/src/main/java/com/example/boardservice/dto/data/PermissionEntry.java
@@ -1,4 +1,25 @@
 package com.example.boardservice.dto.data;
 
-public class PermissionEntry {
+import com.example.boardservice.dto.models.enums.ActionType;
+import com.example.boardservice.dto.models.enums.EntityType;
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PermissionEntry implements Serializable {
+    @Enumerated(EnumType.STRING)
+    private EntityType entity;
+    @Enumerated(EnumType.STRING)
+    private ActionType action;
 }
--- backend/board-service/src/main/java/com/example/boardservice/dto/request/CreateUpdateRoleRequest.java
+++ backend/board-service/src/main/java/com/example/boardservice/dto/request/CreateUpdateRoleRequest.java
@@ -1,4 +1,19 @@
 package com.example.boardservice.dto.request;
 
-public class CreateUpdateRoleRequest {
-}
+import com.example.boardservice.dto.data.PermissionEntry;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Set;
+
+@Schema(description = "Запрос на создание/обновление роли")
+public record CreateUpdateRoleRequest (
+        @Schema(description = "Название роли", example = "User")
+        String name,
+
+        @Schema(description = "Права роли", example = "ISSUE.VIEW")
+        @NotNull
+        @Size(min = 1, message = "Role must have at least one permission")
+        Set<PermissionEntry> permissions
+) {}
