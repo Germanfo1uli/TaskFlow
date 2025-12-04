1b03596c7cbd1222f967bf827323a56aaf10e2a3
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-04 17:56:45 +0700

    boardservice: create member service and apply code refactoring

---
+++ backend/board-service/src/main/java/com/example/boardservice/exception/AlreadyMemberException.java
@@ -0,0 +1,7 @@
+package com.example.boardservice.exception;
+
+public class AlreadyMemberException extends RuntimeException {
+  public AlreadyMemberException(String message) {
+    super(message);
+  }
+}
