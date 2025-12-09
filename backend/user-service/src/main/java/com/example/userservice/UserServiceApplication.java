35a1f3405b60a18e0928a49f0560cf8c55c8b357
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-09 20:55:23 +0700

    userservice: redis caching set up

--- backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
+++ backend/user-service/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -3,6 +3,7 @@ package com.example.userservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -10,6 +11,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EntityScan(basePackages = "com.example.userservice.dto")
 @EnableAsync
 @EnableDiscoveryClient
+@EnableCaching
 public class UserServiceApplication {
 
     public static void main(String[] args) {
