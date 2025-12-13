75d342e935e36a116f5ee91e6cdc2a7834605390
Krivenyshev Evgeniy <crive.ru@yandex.ru>
2025-12-13 17:03:44 +0700

    boardservice: fixed notblank annotation

--- backend/board-service/src/main/java/com/example/boardservice/dto/request/RoleAssignRequest.java
+++ backend/board-service/src/main/java/com/example/boardservice/dto/request/RoleAssignRequest.java
@@ -2,14 +2,15 @@ package com.example.boardservice.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Schema(description = "Запрос на смену роли пользователя")
 public record RoleAssignRequest(
         @Schema(description = "Id пользователя", example = "123")
-        @NotBlank(message = "UserId is required")
+        @NotNull(message = "UserId is required")
         Long userId,
 
         @Schema(description = "Id роли", example = "123")
-        @NotBlank(message = "RoleId is required")
+        @NotNull(message = "RoleId is required")
         Long roleId
 ) {}
